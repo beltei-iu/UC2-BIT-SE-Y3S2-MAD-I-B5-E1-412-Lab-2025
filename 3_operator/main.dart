c73da636a4350a5b1ae0e0a1c3fdbc09cc0cc1d2
Chhai Chivon <chivon.chhai.kh@gmail.com>
2025-01-16 18:51:05 +0700

    3_operator

---
+++ 3_operator/main.dart
@@ -0,0 +1,56 @@
+
+void main(){
+  // Arithmetic Operators
+  int a = 3;
+  int b = 2;
+  // print("Addition: ${a + b}");
+  // print("Subtraction: ${a - b}");
+  // print("Multiplication: ${a * b}");
+  // print("Division:  ${a / b}");
+  // print("Modulus: $a % $b : ${a % b}");
+  // print("Increment: ${a++}");
+  // print("Decrement: ${b--}");
+  
+  // Relational Operators
+  // print("Equal: $a == $b :  ${a == b}");
+  // print("Not Equal: $a != $b :  ${a != b}");
+  // print("Greater Than: $a > $b :  ${a > b}");
+  // print("Less Than: $a < $b :  ${a < b}");
+  // print("Greater Than or Equal: $a >= $b :  ${a >= b}");
+  // print("Less Than or Equal: $a <= $b :  ${a <= b}");
+  
+  // Logical Operators
+  // bool x = true;
+  // bool y = false;
+  // print("AND: $x && $y : ${x && y}");
+  // print("AND: $x && $x : ${x && x}");
+  // print("OR: $x || $y : ${x || y}");
+  // print("OR: $y || $y : ${y || y}");
+  // print("NOT: !$x : ${!x}");
+  
+  // Bitwise Operators
+  // int p = 2; // 10
+  // int q = 3; // 11
+  // print("AND: ${p & q}"); // 10
+  // print("OR: ${p | q}"); // 11
+  // print("XOR: ${p ^ q}"); // 01
+  // print("NOT: ${~p}"); // 01
+  // print("Left Shift: ${p << 1}"); // 100
+  // print("Right Shift: ${p >> 1}"); // 1
+  
+  // Assignment Operators
+  // dynamic c = 10;
+  // print("C: $c");
+  // c += 2; // c = c + 2
+  // print("C: $c");
+  // c -= 2; // c = c - 2
+  // print("C: $c");
+  // c *= 2; // c = c * 2
+  // print("C: $c");
+  // c /= 2; // c = c / 2
+  // print("C: $c");
+
+  // Conditional Operators
+  bool single = true;
+  print("Status: ${single ? 'Single' : 'Married'}");
+}
